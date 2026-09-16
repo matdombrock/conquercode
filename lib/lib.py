d2d871017970f1b20b485df360f123f4a9294e03
matdombrock <dombrock.mathieu@gmail.com>
2026-09-15 22:56:35 -0700

    python add types

--- lib/lib.py
+++ lib/lib.py
@@ -1,11 +1,11 @@
 import sys
 
 
-def get_input():
+def get_input() -> list[str]:
     return sys.argv[1:]
 
 
-def get_input_n():
+def get_input_n() -> list[int]:
     return [int(v) for v in sys.argv[1:]]
 
 
